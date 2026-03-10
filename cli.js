fd547897867ad0e6d95508916a2314424fdea3b4
janklubus <jan@klubus.cz>
2026-03-10 17:56:46 +0100

    Apply prettier

--- cli.js
+++ cli.js
@@ -3,19 +3,16 @@ import { packageInfo } from './config.js';
 
 const command = new Command();
 
-command
-    .name(packageInfo.name)
-    .description(packageInfo.description)
-    .version(packageInfo.version);
+command.name(packageInfo.name).description(packageInfo.description).version(packageInfo.version);
 
 command
-    .option('-r, --remote <url>', 'URL of the remote Eshop with https:// prefix')
-    .option('-w, --watch', 'watch for changes and reload the page', true)
-    .option('-b, --blankMode', 'simulate the blank template.', false)
-    .option('-n, --notify', 'display pop-over notifications in the browser', false)
-    .option('-rh, --removeHeaderIncludes [removeHeaderIncludes...]', 'remove header includes', false)
-    .option('-rf, --removeFooterIncludes [removeFooterIncludes...]', 'remove footer includes', false)
-    .option('-bl, --builder <type>', 'Specify the builder to use (webpack or gulp)')
-    .option('-o, --open', 'open browser after first build process', false);
+  .option('-r, --remote <url>', 'URL of the remote Eshop with https:// prefix')
+  .option('-w, --watch', 'watch for changes and reload the page', true)
+  .option('-b, --blankMode', 'simulate the blank template.', false)
+  .option('-n, --notify', 'display pop-over notifications in the browser', false)
+  .option('-rh, --removeHeaderIncludes [removeHeaderIncludes...]', 'remove header includes', false)
+  .option('-rf, --removeFooterIncludes [removeFooterIncludes...]', 'remove footer includes', false)
+  .option('-bl, --builder <type>', 'Specify the builder to use (webpack or gulp)')
+  .option('-o, --open', 'open browser after first build process', false);
 
 export default command;
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 /** @format */
 
-import command from "./cli.js";
-import { config } from "./config.js";
-import fs from "fs";
-import path from "path";
-import BrowserSync from "browser-sync";
-import BrowserSyncPlugin from "browser-sync-webpack-plugin";
-import webpack from "webpack";
-import getWebpackConfig from "./webpack.config.js";
+import command from './cli.js';
+import { config } from './config.js';
+import fs from 'fs';
+import path from 'path';
+import BrowserSync from 'browser-sync';
+import BrowserSyncPlugin from 'browser-sync-webpack-plugin';
+import webpack from 'webpack';
+import getWebpackConfig from './webpack.config.js';
 
-const localGulpfile = path.join(process.cwd(), "gulpfile.js");
+const localGulpfile = path.join(process.cwd(), 'gulpfile.js');
 let build, watch;
 
 if (fs.existsSync(localGulpfile)) {
-    console.log("Using local gulpfile.js");
-    ({ build, watch } = await import(localGulpfile));
+  console.log('Using local gulpfile.js');
+  ({ build, watch } = await import(localGulpfile));
 } else {
-    console.log("Using global gulpfile.js");
-    ({ build, watch } = await import("./gulpfile.js"));
+  console.log('Using global gulpfile.js');
+  ({ build, watch } = await import('./gulpfile.js'));
 }
 
 command.parse(process.argv);
@@ -27,127 +27,141 @@ const options = command.opts();
 
 const rootDir = process.cwd();
 
-const sourceFolder = path.join(rootDir, config.sourceFolder ?? "src");
+const sourceFolder = path.join(rootDir, config.sourceFolder ?? 'src');
 const outputFolder = path.join(rootDir, config.outputFolder);
 
 const includesRegex = /<!-- (?:service\s\d+\(\d+\)|api\s\d+\(\d+\)|project)\shtml\s+code\s+(?:header|footer) -->/gi;
-const includesCodeRegex = new RegExp(includesRegex.source + "[\\s\\S]*?(?=" + includesRegex.source + "|$)", "g");
+const includesCodeRegex = new RegExp(includesRegex.source + '[\\s\\S]*?(?=' + includesRegex.source + '|$)', 'g');
 
 const blankModeStyle = {
-    match: /<link\s+href="https:\/\/cdn\.myshoptet\.com\/prj\/[^"]+"[^>]*>/gi,
-    fn: function () {
-        return "";
-    },
+  match: /<link\s+href="https:\/\/cdn\.myshoptet\.com\/prj\/[^"]+"[^>]*>/gi,
+  fn: function () {
+    return '';
+  },
 };
 
 const blankModeScript = {
-    match: /<script\s+src="https:\/\/cdn\.myshoptet\.com\/prj\/[^"]+"[^>]*>/gi,
-    fn: function () {
-        return "";
-    },
+  match: /<script\s+src="https:\/\/cdn\.myshoptet\.com\/prj\/[^"]+"[^>]*>/gi,
+  fn: function () {
+    return '';
+  },
 };
 
 const headerIncludes = {
-    match: /(?<=<head[\s\S]*?<!--\sUser include\s-->)[\s\S]*?(?=<!--\s\/User include\s-->)/i,
-    fn: function (req, res, match) {
-        // Remove includes from the header
-        const includes = options.removeHeaderIncludes || config.removeHeaderIncludes || [];
-        const matchedServices = match.match(includesCodeRegex);
-        if (matchedServices) {
-            match = matchedServices
-                .filter((service) => {
-                    return !includes.some((removedService) => service.includes(removedService));
-                })
-                .join("");
-        }
+  match: /(?<=<head[\s\S]*?<!--\sUser include\s-->)[\s\S]*?(?=<!--\s\/User include\s-->)/i,
+  fn: function (req, res, match) {
+    // Remove includes from the header
+    const includes = options.removeHeaderIncludes || config.removeHeaderIncludes || [];
+    const matchedServices = match.match(includesCodeRegex);
+    if (matchedServices) {
+      match = matchedServices
+        .filter(service => {
+          return !includes.some(removedService => service.includes(removedService));
+        })
+        .join('');
+    }
 
-        // Add custom includes to the footer
-        const headerMarkup =
-            (fs.existsSync(outputFolder + "/scripts.header.js") ? '<script data-type="shp-bender-include" src="/scripts.header.js"></script>' : "") +
-            (fs.existsSync(outputFolder + "/styles.header.css") ? '<link data-type="shp-bender-include" rel="stylesheet" href="/styles.header.css">' : "");
-        return match + headerMarkup;
-    },
+    // Add custom includes to the footer
+    const headerMarkup =
+      (fs.existsSync(outputFolder + '/scripts.header.js')
+        ? '<script data-type="shp-bender-include" src="/scripts.header.js"></script>'
+        : '') +
+      (fs.existsSync(outputFolder + '/styles.header.css')
+        ? '<link data-type="shp-bender-include" rel="stylesheet" href="/styles.header.css">'
+        : '');
+    return match + headerMarkup;
+  },
 };
 
 const footerIncludes = {
-    match: /(?<=<body[\s\S]*?<!--\sUser include\s-->\s*<div class="container">)[\s\S]*?(?=<\/div>\s*<!--\s\/User include\s-->)/i,
-    fn: function (req, res, match) {
-        // Remove includes from the footer
-        const includes = options.removeFooterIncludes || config.removeFooterIncludes || [];
-        const matchedServices = match.match(includesCodeRegex);
-        if (matchedServices) {
-            match = matchedServices
-                .filter((service) => {
-                    return !includes.some((removedService) => service.includes(removedService));
-                })
-                .join("");
-        }
+  match:
+    /(?<=<body[\s\S]*?<!--\sUser include\s-->\s*<div class="container">)[\s\S]*?(?=<\/div>\s*<!--\s\/User include\s-->)/i,
+  fn: function (req, res, match) {
+    // Remove includes from the footer
+    const includes = options.removeFooterIncludes || config.removeFooterIncludes || [];
+    const matchedServices = match.match(includesCodeRegex);
+    if (matchedServices) {
+      match = matchedServices
+        .filter(service => {
+          return !includes.some(removedService => service.includes(removedService));
+        })
+        .join('');
+    }
 
-        // Add custom includes to the footer
-        const footerMarkup =
-            (fs.existsSync(outputFolder + "/scripts.footer.js") ? '<script data-type="shp-bender-include" src="/scripts.footer.js"></script>' : "") +
-            (fs.existsSync(outputFolder + "/styles.footer.css") ? '<link data-type="shp-bender-include" rel="stylesheet" href="/styles.footer.css">' : "");
-        return match + footerMarkup;
-    },
+    // Add custom includes to the footer
+    const footerMarkup =
+      (fs.existsSync(outputFolder + '/scripts.footer.js')
+        ? '<script data-type="shp-bender-include" src="/scripts.footer.js"></script>'
+        : '') +
+      (fs.existsSync(outputFolder + '/styles.footer.css')
+        ? '<link data-type="shp-bender-include" rel="stylesheet" href="/styles.footer.css">'
+        : '');
+    return match + footerMarkup;
+  },
 };
 
-const rewriteRules = [{ ...headerIncludes }, { ...footerIncludes }, { ...(options.blankMode && blankModeStyle) }, { ...(options.blankMode && blankModeScript) }];
+const rewriteRules = [
+  { ...headerIncludes },
+  { ...footerIncludes },
+  { ...(options.blankMode && blankModeStyle) },
+  { ...(options.blankMode && blankModeScript) },
+];
 
 function runWebpack() {
-    const bsPlugin = [
-        new BrowserSyncPlugin({
-            proxy: { target: options.remote ?? config.defaultUrl },
-            serveStatic: [outputFolder],
-            rewriteRules: rewriteRules.filter((value) => Object.keys(value).length !== 0),
-            port: 3010,
-            notify: options.notify || config.notify || true,
-            open: options.open || config.open || false,
-        }),
-    ];
+  const bsPlugin = [
+    new BrowserSyncPlugin({
+      proxy: { target: options.remote ?? config.defaultUrl },
+      serveStatic: [outputFolder],
+      rewriteRules: rewriteRules.filter(value => Object.keys(value).length !== 0),
+      port: 3010,
+      notify: options.notify || config.notify || true,
+      open: options.open || config.open || false,
+    }),
+  ];
 
-    const baseWebpackConfig = getWebpackConfig(options.env ?? config.env);
-    const webpackConfig = {
-        watch: options.watch,
-        ...baseWebpackConfig,
-        plugins: [...bsPlugin, ...baseWebpackConfig.plugins],
-    };
-    webpack(webpackConfig, (err, stats) => {
-        if (err || stats.hasErrors()) {
-            console.log("Webpack errors", err);
-        }
-        console.log(stats.toString({ colors: true }));
-    });
+  const baseWebpackConfig = getWebpackConfig(options.env ?? config.env);
+  const webpackConfig = {
+    watch: options.watch,
+    ...baseWebpackConfig,
+    plugins: [...bsPlugin, ...baseWebpackConfig.plugins],
+  };
+  webpack(webpackConfig, (err, stats) => {
+    if (err || stats.hasErrors()) {
+      console.log('Webpack errors', err);
+    }
+    console.log(stats.toString({ colors: true }));
+  });
 }
 
 async function runGulpTask() {
-    try {
-        await build();
-    } catch (err) {
-        console.error("Gulp task error:", err);
-    }
+  try {
+    await build();
+  } catch (err) {
+    console.error('Gulp task error:', err);
+  }
 }
 
 async function runGulp() {
-    watch();
-    runGulpTask();
-    const bs = BrowserSync({
-        proxy: { target: options.remote ?? config.defaultUrl },
-        watch: options.watch,
-        files: [outputFolder],
-        serveStatic: [outputFolder],
-        rewriteRules: rewriteRules.filter((value) => Object.keys(value).length !== 0),
-        port: 3010,
-        notify: options.notify || config.notify || true,
-        open: options.open || config.open || false,
-    });
+  watch();
+  runGulpTask();
+  const bs = BrowserSync({
+    proxy: { target: options.remote ?? config.defaultUrl },
+    watch: options.watch,
+    files: [outputFolder],
+    serveStatic: [outputFolder],
+    rewriteRules: rewriteRules.filter(value => Object.keys(value).length !== 0),
+    port: 3010,
+    notify: options.notify || config.notify || true,
+    open: options.open || config.open || false,
+  });
 }
 
-const builder = options.builder || config.builder || "webpack";
+const builder = options.builder || config.builder || 'webpack';
 
-if (builder === "webpack") {
-    runWebpack();
-} else if (builder === "gulp") {
-    await runGulp();
+if (builder === 'webpack') {
+  runWebpack();
+} else if (builder === 'gulp') {
+  await runGulp();
 } else {
-    console.error("Please specify a valid builder: webpack or gulp");
+  console.error('Please specify a valid builder: webpack or gulp');
 }
